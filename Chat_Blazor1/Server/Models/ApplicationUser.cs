bdb4ba4cae07cf9a4b0a214d0947c2104370dbe3
ScottTomassetti <scott_t_ct@hotmail.com>
2020-04-12 12:19:19 -0400

    Add project files.

---
+++ Chat_Blazor1/Server/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chat_Blazor1.Server.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
